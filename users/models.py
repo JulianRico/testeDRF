ce7da679356c42d29772602ef679eef9f6158949
Julian Quintero <julianandres.quinterorico@emeal.nttdata.com>
2023-04-12 15:54:47 -0500

    firts comit

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=500)
+    email = models.EmailField()
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    about = models.TextField()
+    pictureurl = models.URLField()
+    position = models.CharField(max_length=200)
+    create_at = models.DateTimeField(auto_now_add=True)

968abd4d5f2e93be4204000170e870090a252b13
Julian Quintero <julianandres.quinterorico@emeal.nttdata.com>
2023-06-07 15:08:51 -0500

    prueba de super admin

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+# Configuración de variables
+PYTHON_INTERPRETER=python
+DJANGO_MANAGE_PY=manage.py
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+$PYTHON_INTERPRETER $DJANGO_MANAGE_PY createsuperuser
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('jquintero', 'juliquinterorico@hotmail.com', 'Qwaszx.123')" | $PYTHON_INTERPRETER $DJANGO_MANAGE_PY shell

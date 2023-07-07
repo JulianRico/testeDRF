7b5848e33f0e1c0863d81a8accd32683c584cdc9
Julian Quintero <julianandres.quinterorico@emeal.nttdata.com>
2023-07-07 10:50:51 -0500

    elimina superuser

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 $PYTHON_INTERPRETER $DJANGO_MANAGE_PY createsuperuser
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('jquintero', 'juliquinterorico@hotmail.com', 'Qwaszx.123')" | $PYTHON_INTERPRETER $DJANGO_MANAGE_PY shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(is_superuser=True).delete();  User.objects.create_superuser('jquintero', 'juliquinterorico@hotmail.com', 'Qwaszx.123')" | $PYTHON_INTERPRETER $DJANGO_MANAGE_PY shell

b9473041e494a6c608e78b9211261f774aa68d60
Julian Quintero <julianandres.quinterorico@emeal.nttdata.com>
2023-08-01 10:15:48 -0500

    add usercompany

--- companies/serializers.py
+++ companies/serializers.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 from .models import Companie
 from reports.serializers import ReportSerializer
 
+
 class CompanieSerializer (serializers.ModelSerializer):
     reports = ReportSerializer(many=True, required=False)
+
     class Meta:
         model = Companie
-        fields = ('id', 'name','email', 'address', 'phone','nit','manager','create_at', 'reports')
-        read_only_fields = ('create_at',)        
-        
+        fields = ('id', 'name', 'email', 'address', 'phone', 'nit',
+                  'manager', 'userCompany', 'create_at', 'reports')
+        read_only_fields = ('create_at',)

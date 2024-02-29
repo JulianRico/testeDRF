c9e9ae3f9336f640b657b628eafff56ff21c250c
[julianrico] <[juliquinterorico@hotmail.com]>
2024-02-29 15:50:43 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-apt-get install -y \
+#!/bin/bash
+
+# Actualiza los repositorios e instala las dependencias necesarias
+apt-get update && apt-get install -y \
     wget \
     xvfb \
     libfontconfig \
@@ -10,20 +14,24 @@ apt-get install -y \
     libxext6 \
     libxrender1 \
     xfonts-75dpi \
-    xfonts-base \
+    xfonts-base
+
+# Descarga e instala wkhtmltopdf
+wget -q -O wkhtmltopdf.deb https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb && \
+    dpkg -i wkhtmltopdf.deb && \
+    apt-get install -f
     
-# Instala LibreOffice
+apt-get install libreoffice
+# Limpia el sistema
+apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* wkhtmltopdf.deb
 
-
-# Instala las dependencias de Python
+# Configuración de variables
 PYTHON_INTERPRETER=python
 DJANGO_MANAGE_PY=manage.py
 pip install -r requirements.txt
 
-# Ejecuta comandos de Django
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-
-# Crea un superusuario de Django (cambia los valores según sea necesario)
+$PYTHON_INTERPRETER $DJANGO_MANAGE_PY createsuperuser
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(is_superuser=True).delete();  User.objects.create_superuser('jquintero', 'juliquinterorico@hotmail.com', 'Qwaszx.123')" | $PYTHON_INTERPRETER $DJANGO_MANAGE_PY shell

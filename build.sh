6e982708e1ac5a95e4df76a5e890d124e3196db5
[julianrico] <[juliquinterorico@hotmail.com]>
2024-03-01 10:36:31 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+#!/bin/bash
+
 # Actualiza los repositorios e instala las dependencias necesarias
 apt-get update && apt-get install -y \
     wget \
@@ -14,24 +16,22 @@ apt-get update && apt-get install -y \
     xfonts-75dpi \
     xfonts-base
 
-# Descarga e instala LibreOffice
-wget -q -O LibreOffice.tar.gz https://download.documentfoundation.org/libreoffice/stable/7.2.1/deb/x86_64/LibreOffice_7.2.1_Linux_x86-64_deb.tar.gz && \
-tar -zxvf LibreOffice.tar.gz && \
-cd LibreOffice_7.2.1.2_Linux_x86-64_deb/DEBS/ && \
-sudo dpkg -i *.deb && \
-sudo apt-get install -f
+    
+wget -q -O LibreOffice_24.2.1_Linux_x86-64_deb.tar.gz https://download.documentfoundation.org/libreoffice/stable/24.2.1/deb/x86_64/LibreOffice_24.2.1_Linux_x86-64_deb.tar.gz && \
+tar -zxvf LibreOffice_24.2.1_Linux_x86-64_deb.tar.gz && \
+cd LibreOffice_24.2.1.2_Linux_x86-64_deb/DEBS/ && \
+dpkg -i *.deb && \
+apt-get install -f
 
-# Limpia los archivos temporales
-apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* LibreOffice.tar.gz LibreOffice_7.2.1.2_Linux_x86-64_deb
 
-# Configura las variables
+apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* wkhtmltopdf.deb LibreOffice_24.2.1_Linux_x86-64_deb.tar.gz LibreOffice_24.2.1_Linux_x86-64_deb
+
+
+# Configuración de variables
 PYTHON_INTERPRETER=python
 DJANGO_MANAGE_PY=manage.py
-
-# Instala las dependencias de Python
 pip install -r requirements.txt
 
-# Configuración adicional de Django
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
